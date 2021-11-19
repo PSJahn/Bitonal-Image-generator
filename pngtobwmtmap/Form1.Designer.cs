5ed9abf7aeb822ea07182594b9d565a05b932219
psjah <psjah@AGENTDARK>
2021-11-19 17:21:51 +0100

    Add project files.

---
+++ pngtobwmtmap/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿
+namespace pngtobwmtmap
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Select Image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(13, 56);
+            this.trackBar1.Maximum = 255;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(114, 45);
+            this.trackBar1.TabIndex = 1;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 122);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Contrast";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Preview";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 229);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Output";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(13, 247);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(100, 67);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 350);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(114, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Generate Output";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(9, 408);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(56, 19);
+            this.checkBox1.TabIndex = 11;
+            this.checkBox1.Text = "invert";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(13, 320);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 23);
+            this.progressBar1.TabIndex = 12;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(13, 379);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(114, 23);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Save as Image";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(167, 432);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Bitmap Tool";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ pngtobwmtmap/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pngtobwmtmap
+{
+    public partial class Form1 : Form
+    {
+        String imagePath = "empty";
+        OpenFileDialog fd = new OpenFileDialog();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Resource1.preview_empty;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            fd.Multiselect = false;
+            if(fd.ShowDialog().Equals(DialogResult.OK))
+            {
+                imagePath = fd.FileName;
+                updatePath();
+            }
+        }
+
+        List<Color> pixels = new List<Color>();
+
+        private void updatePath()
+        {
+            if (imagePath.Equals("empty"))
+            {
+                return;
+            }
+            Bitmap img = new Bitmap(imagePath);
+            pixels.Clear();
+
+            for (int j = 0; j < img.Height; j++)
+            {
+                for (int i = 0; i < img.Width; i++)
+                {
+                    Color pixel = img.GetPixel(i, j);
+
+                    if(checkBox1.Checked)
+                    {
+                        if (pixel.R > trackBar1.Value)
+                        {
+                            pixel = Color.White;
+                        }
+                        else
+                        {
+                            pixel = Color.Black;
+                        }
+                    } else
+                    {
+                        if (pixel.R > trackBar1.Value)
+                        {
+                            pixel = Color.Black;
+                        }
+                        else
+                        {
+                            pixel = Color.White;
+                        }
+                    }
+                    pixels.Add(pixel);
+                }
+            }
+        
+            int x = 0;
+            int y = 0;
+            //img.LockBits();
+            foreach(Color pixel in pixels)
+            {
+                y = x / img.Width;
+                y = (int)y;
+                img.SetPixel(x-(img.Width*y), y, pixel);
+                x++;
+            }
+            //img.UnlockBits();   
+            pictureBox1.Image = img;
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            updatePath();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+            Bitmap bmp = new Bitmap(pictureBox1.Image);
+            bmp.Save(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\img.png", ImageFormat.Png);
+            MessageBox.Show("Saved to desktop!");
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            updatePath();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string text = "P1\n" + pictureBox1.Image.Width + " " + pictureBox1.Image.Height + "\n";
+            int x = 0;
+            int y = 0;
+            int yprev = 0;
+            int progress = 0;
+            progressBar1.Maximum = pixels.Count;
+            foreach (Color pixel in pixels)
+            {
+                
+                y = x / pictureBox1.Image.Width;
+                y = (int)y;
+                if(y!=yprev)
+                {
+                    text = text.Substring(0,text.Length-1);
+                    text += "\n";
+                    yprev = y;
+                }
+                int asd = Color.White == pixel ? asd = 0 : asd = 1;
+                text += asd + " ";
+                //img.SetPixel(x - (img.Width * y), y, pixel);
+                x++;
+                progress++;
+                progressBar1.Value = progress;
+                Application.DoEvents();
+            }
+            richTextBox1.Text = text;
+        }
+    }
+}
